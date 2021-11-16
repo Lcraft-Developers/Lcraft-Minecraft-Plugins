d07689c42761685f93786adff7da5bb05399d49c
LPDMinecraft <l_jost@online.de>
2021-11-16 07:48:52 +0100

    Added Lessentials

---
+++ Lessential Plugin (Essentials)/src/main/java/de/lcraft/plugins/lessentials/LessentialsPlugin.java
@@ -0,0 +1,24 @@
+package de.lcraft.plugins.lessentials;
+
+import de.lcraft.api.utils.minecraft.spigot.module.Module;
+
+import java.io.IOException;
+
+public final class LessentialsPlugin extends Module {
+
+    @Override
+    public void onLoad() throws IOException {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
